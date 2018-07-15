84ff24f2ffd07e6fde96b407e4d4dfafd3ad8283
benli615349 <39723820+benli615349@users.noreply.github.com>
2018-07-16 09:47:09 +1000

    Update 001-Two-Sum.js

--- 001-Two-Sum.js
+++ 001-Two-Sum.js
@@ -15,14 +15,6 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (numbers, target) {
-
-    for (var i = 0; i < numbers.length - 1; i++) {
-        for (var j = i + 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) return [i, j];
-        }
-    }
-};
 
 var twoSum2 = function (numbers, target) {
     var map = {};
@@ -36,7 +28,6 @@ var twoSum2 = function (numbers, target) {
     }
 };
 
-console.log(twoSum([2, 11, 15, 7], 9));
 console.log(twoSum2([2, 7, 11, 15], 9));
 console.log(twoSum2([2, 7, 11, 15], 26));
 console.log(twoSum2([2, 7, 11, 15], 26));
