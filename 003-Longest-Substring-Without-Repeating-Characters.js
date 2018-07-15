ce702c91abb15a8a564cc9385b9ecc7929a573a3
benli615349 <39723820+benli615349@users.noreply.github.com>
2018-07-16 09:23:11 +1000

    Update 003-Longest-Substring-Without-Repeating-Characters.js

--- 003-Longest-Substring-Without-Repeating-Characters.js
+++ 003-Longest-Substring-Without-Repeating-Characters.js
@@ -10,32 +10,34 @@
  *
  */
 
-/**
- * @param {string} s
- * @return {number}
- */
+分析：用两个指针ij从头开始遍历字符串。ij分别表示最长字串的首尾下标。
+如果第j个与i与j当中的某处k重复，那么只需i从k+1开始继续判断是否有重复的就好。
+当然，在i++一直到k的过程中，不要忘记把已经收录的字符存在标记为不存在。
+所以用一个book数组标记该字符在i~j当中是否出现过。每一次找到重复的字符的时候判断j-i是否比最大值大。
+一个特例是，如果i~j中一直让j到了最后一个字符都没有重复但是此时的j-i是最长的长度，所以要在return语句前再加上一句判断j-i的大小是否比当前maxlen大。
+因为i和j都只遍历了字符串一次，所以时间复杂度为O(n)。
 
-    public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int i = 0, j = 0;
-        int maxLen = 0;
-        boolean[] exists = new boolean[256];
-        while(j < n){
-            if(exists[s.charAt(j)]){
-                // met the repeat character, update the maxlen;
-                maxLen = Math.max(maxLen, j - i);
-                // update pointer i
-                while(s.charAt(i) != s.charAt(j)){
-                    exists[s.charAt(i)] = false;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i = 0, j = 0, len = s.length();
+        int maxlen = 0;
+        int book[256] = {0};
+        while(j < len) {
+            if(book[s[j]] == 1) {
+                maxlen = max(maxlen, j - i);
+                while(s[i] != s[j]) {
+                    book[s[i]] = 0;
                     i++;
                 }
                 i++;
-                j++;
             } else {
-                exists[s.charAt(j)] = true;
-                j++;
+                book[s[j]] = 1;
             }
+            j++;
         }
-        maxLen = Math.max(maxLen, n - i);
-        return maxLen; 
+        maxlen = max(maxlen, j - i);
+        return maxlen;
     }
+};
