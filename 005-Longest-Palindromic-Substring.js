46ff473622c43e090a62a6cb87ed4f832f20a831
benli615349 <39723820+benli615349@users.noreply.github.com>
2018-07-24 23:14:40 +1000

    Update 005-Longest-Palindromic-Substring.js

--- 005-Longest-Palindromic-Substring.js
+++ 005-Longest-Palindromic-Substring.js
@@ -18,36 +18,38 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function (s) {
-    var a = new Date();
-    var n = s.length;
-    var res = '';
-    var dp = [];
-    while (dp.push(new Array(n).fill(-1)) < n);
-    // console.log(dp);
 
-    for (var i = n - 1; i >= 0; i--) {
-        for (var j = i; j < n; j++) {
-            dp[i][j] = s[i] === s[j] && ((j - i < 3) || dp[i + 1][j - 1]);
-            if (dp[i][j] === undefined) {
-                console.log(i, j, s[i], s[j], dp[i + 1][j - 1])
-            }
-            if (dp[i][j]) {
-                var tmp = s.substring(i, j + 1);
-                if (tmp.length > res.length) res = tmp;
-            }
+必须使用DP解决问题，其他的没意思
+我们维护一个二维数组dp，其中dp[i][j]表示字符串区间[i, j]是否为回文串
+有且仅有三种情况
+i=j，那么只有一个字符，dp[i][j] = 1
+i + 1 = j，dp[i][j] = s[i] === s[j]
+j - i > 1, dp[i][j]的值取决于dp[i+1][j-1] && s[i] === s[j]
 
+var longestPalindrome = function(s) {
+    if (s === '' || s.length === 0) {
+        return ''
+    }
+    
+    const n = s.length;
+    
+    let dp = [];
+    while(dp.push(new Array(n).fill(0)) < n);
+    
+    let i, j, maxLen = 0;
+    let result = '';
+    
+    for(i = 0; i < n; i++) {
+        for(j = 0; j <= i; j++) {
+            if (i === j) dp[i][j] = 1;
+            if ((i - j) === 1) dp[i][j] = s[i] === s[j];
+            if(i - j > 1) dp[i][j] = dp[i-1][j+1] && s[i] === s[j];
+            let distanceBetweenIandJ = i - j + 1;
+            if(dp[i][j] && maxLen < distanceBetweenIandJ) {
+                maxLen = distanceBetweenIandJ;
+                result = s.substring(j, i + 1)
+            }
         }
     }
-    // console.log(dp);
-    console.log(new Date() - a);
-
-    return res;
+    return result;
 };
-
-// console.log(isPalindrome(s, 1, 3));
-// console.log(longestPalindrome('babad'));
-// console.log(longestPalindrome(''));
-// console.log(longestPalindrome('a'));
-// console.log(longestPalindrome('aabbbbbb'));
-console.log(longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
