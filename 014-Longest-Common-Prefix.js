01d1fd842cd9e1561f20eb1964674e0af598607b
benli615349 <39723820+benli615349@users.noreply.github.com>
2018-08-01 22:39:05 +1000

    Update 014-Longest-Common-Prefix.js

--- 014-Longest-Common-Prefix.js
+++ 014-Longest-Common-Prefix.js
@@ -1,50 +1,48 @@
-/**
- * https://leetcode.com/problems/longest-common-prefix/
- * Difficulty:Easy
- *
- * Write a function to find the longest common prefix string amongst an array of strings.
- */
 
-/**
- * @param {string[]} strs
- * @return {string}
- */
-var longestCommonPrefix = function (strs) {
 
-    var m = strs.length;
-    if (!m) return '';
+// Write a function to find the longest common prefix string amongst an array of strings.
 
-    var min = Infinity;
-    var minIndex = -1;
-    for (var i = 0; i < strs.length; i++) {
-        if (strs[i].length < min) {
-            min = strs[i].length;
-            minIndex = i;
-        }
-    }
+// If there is no common prefix, return an empty string "".
 
-    var s = strs[minIndex];
+// Example 1:
 
-    for (i = 0; i < s.length; i++) {
-        var ch = strs[0][i];
-        var same = true;
-        for (var j = 1; j < m; j++) {
-            if (strs[j][i] !== ch) {
-                same = false;
+// Input: ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+// Note:
+
+// All given inputs are in lowercase letters a-z.
+
+比對前兩個字串，從頭開始取出相同的部分為共同字首
+後面的字串只要與目前的共同字首比對即可
+['abcd','abccc','abdec'] ，一開始'abcd','abccc'共同字首前3碼'abc'
+接下來只要將'abc','abdec'做比對，發現剩下'ab'，也就是最長的共同字首
+
+
+var longestCommonPrefix = function(strs) {
+    if(strs == null || strs.length == 0) return "";
+
+    // same表示目前發現的共同字首，一開始為strs[0]
+    var same = strs[0];
+
+    // 只需要比對same跟目前字串共同的字元就好
+    for(var i = 1 ; i<strs.length ; i++){
+        var str = strs[i];
+
+        // 取目前的字串str跟same相等的部分做為新的same
+        var j = 0;
+        for(; j < same.length ; j++){
+           if(same[j] != str.charAt(j)){
                 break;
-            }
-
+           } 
         }
-        if (!same) break;
-
+        // same與目前字串str前幾位相同，就做為新的same
+        same = same.slice(0,j);
     }
 
-    return s.substr(0, i);
-
+    return same;
 };
-
-console.log(longestCommonPrefix([
-    'abdc',
-    'abc123',
-    'abc234'
-]));
