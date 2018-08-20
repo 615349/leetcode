0e0c2a488151c5395cc6ea005624428fe7f07e8e
benli615349 <39723820+benli615349@users.noreply.github.com>
2018-08-20 16:27:16 +1000

    Update 001-Two-Sum.js

--- 001-Two-Sum.js
+++ 001-Two-Sum.js
@@ -22,7 +22,7 @@ for example:
 map = {
    2: 0
 }
-for the next iteration, check if target - the new iterated number is existed as key. if so, we've found the two numbers
+for the next iteration, check if target - the new iterated number is existed as key. if so, we haveve found the two numbers
 
 var twoSum = function (numbers, target) {
     var map = {};
@@ -34,5 +34,37 @@ var twoSum = function (numbers, target) {
             map[n] = i;
         }
     }
+};
+
+
+另外一种方法是，将数组排序（注意，排序会改变数组本身的，所以需要先保存一下原数组）
+然后用两个指针，一个在头上，一个在尾巴，如果等于就保存两个index，如果小于target，则左边指针向右边移动，反之右边指针向左边移动
+拿到两个index之后，在原来数组查找这两个值原来的index
+var twoSum = function(nums, target) {
+  const origNums = [...nums];
+  nums.sort((a, b) => a - b);
+  let i = 0;
+  let j = nums.length - 1;
+
+  let left, right;
+
+  while (i < j) {
+    if (nums[i] + nums[j] === target) {
+      left = i;
+      right = j;
+      break;
+    } else if (nums[i] + nums[j] < target) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  let result = [];
+  origNums.forEach((num, index) => {
+    if (num === nums[left] || num === nums[right]) {
+      result.push(index);
+    }
+  });
+  return result;
 };
 
