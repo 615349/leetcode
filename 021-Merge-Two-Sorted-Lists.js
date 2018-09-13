347c64026daa70c32280f81b3e1c3252329fbc65
Huan Li <39723820+benli615349@users.noreply.github.com>
2018-09-13 14:03:57 +1000

    Update 021-Merge-Two-Sorted-Lists.js

--- 021-Merge-Two-Sorted-Lists.js
+++ 021-Merge-Two-Sorted-Lists.js
@@ -10,6 +10,8 @@
  * Output: 1->1->2->3->4->4
  */
 
+非常基本的题目，不多废话了
+
 
 // Definition for singly-linked list.
 function ListNode(val) {
@@ -46,26 +48,5 @@ var mergeTwoLists = function (l1, l2) {
     if (l2) n.next = l2;
 
     return t.next;
-};
-
-/**
- *
- * recursion
- *
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var mergeTwoLists = function (l1, l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-
-    if (l1.val <= l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
-    }
 };
 
