6a0cc3524633e4c017603f5e6457d8736950fda9
Huan Li <39723820+benli615349@users.noreply.github.com>
2018-10-15 16:03:34 +1100

    Update 030-Substring-with-Concatenation-of-All-Words.js

--- 030-Substring-with-Concatenation-of-All-Words.js
+++ 030-Substring-with-Concatenation-of-All-Words.js
@@ -17,6 +17,37 @@
  * @param {string[]} words
  * @return {number[]}
  */
+
+给定一个字符串，和一个数组words，数组里的string都是相同长度的
+题目的要求是，求出一个连续的子字符串，包含words里所有的单词
+
+解决方法是sliding window
+
+barfoothefoobarman
+|     |
+i1   i2
+
+let m = words.length;
+let n = words[0].length
+
+i从0开始，每次增加 m*n, i <= string.length - m*n, 因为剩下的是m*n - 1, 从sliding window的角度看，剩余的已经不可能复合条件了
+
+首先有一个map，key是words里面的元素，value是出现的次数
+{
+    'foo': 1,
+    'bar': 1
+}
+
+然后在i1和i2之间，使用一个变量j，每次增加n, i.e.
+j = i1
+j = i1 + n
+j = i1 + 2n
+...
+j = i1 + (m-1)*n
+
+在j的循环中，使用第二个map，要求每个词都曾经在第一个map里出现。同时出现的次数也一样。
+如果这些条件都符合，就把i放到result的数组中
+
 var findSubstring = function (s, words) {
     if (!s.length || !words.length) return [];
     var ans = [];
@@ -45,5 +76,3 @@ var findSubstring = function (s, words) {
     return ans;
 
 };
-
-console.log(findSubstring('barfoothefoobarman', ['foo', 'bar']));
