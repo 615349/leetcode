1ec620c99cd74bbc79964de39d85b88b06f464ea
Ben Li <ben.li@qantas.com.au>
2018-10-17 13:44:12 +1100

    034 updated

--- 034-Search-for-a-Range.js
+++ 034-Search-for-a-Range.js
@@ -16,3 +16,50 @@
  * @param {number} target
  * @return {number[]}
  */
+
+要求复杂度是o(logn)的一定是使用binary search 二分法
+因为是已经排序好的递增数组，那么使用两次二分法，
+第一次找到左边的区间，第二次找到右边的区间
+
+class Solution {
+public:
+    vector<int> searchRange(int A[], int n, int target) {
+       if(n == 0) {
+            return vector<int>({-1, -1});
+        }
+
+        vector<int> v;
+        int low = 0;
+        int high = n - 1;
+        //第一次二分找第一个位置
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(A[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if(low < n && A[low] == target) {
+            v.push_back(low);
+        } else {
+            return vector<int>({-1, -1});
+        }
+
+        low = low;
+        high = n - 1;
+        //从第一个位置开始进行第二次二分，找最后一个位置
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(A[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        v.push_back(high);
+        return v;
+    }
+};
