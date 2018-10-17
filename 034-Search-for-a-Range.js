23f46cbc468a10fbd1a39755aed4ee3b64431207
Huan Li <39723820+benli615349@users.noreply.github.com>
2018-10-17 13:41:33 +1100

    Update 034-Search-for-a-Range.js

--- 034-Search-for-a-Range.js
+++ 034-Search-for-a-Range.js
@@ -16,28 +16,3 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function (nums, target) {
-
-    var i = 0;
-    var j = nums.length - 1;
-    var ret = [-1, -1];
-    while (i < j) {
-        var mid = Math.floor((i + j) / 2);
-        // console.log(i, mid, j);
-        if (nums[mid] < target) i = mid + 1;
-        else j = mid;
-    }
-    if (nums[i] !== target) return ret;
-    ret[0] = i;
-    j = nums.length - 1;
-    while (i < j) {
-        mid = Math.ceil((i + j) / 2);
-        // console.log(i, mid, j);
-        if (nums[mid] > target) j = mid - 1;
-        else i = mid;
-    }
-    ret[1] = j;
-    return ret;
-};
-
-console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
