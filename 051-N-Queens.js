60ac2415adea1d993374669210a740a810179438
Huan Li <39723820+benli615349@users.noreply.github.com>
2018-10-25 22:50:52 +1100

    Update 051-N-Queens.js

--- 051-N-Queens.js
+++ 051-N-Queens.js
@@ -32,46 +32,68 @@
 
 放皇后的位置放Q，其他位置放.
 
+例如：对于一个4皇后问题，声明一个长度为4的数组（因为行数为4）。
+     A[] = [1,0,2,3]表达含义是：
+     当前4个皇后所在坐标点为：[[0,1],[1,0],[2,2],[3,3]](前一位是行数，后一位是位置)
+     相当于：A[0] = 1, A[1] = 0, A[2] = 2, A[3] = 3 
 
+这样以来，皇后所在的坐标值就能用一维数组表示了。
+
+我们不妨从其中一个条件入手——即相同的一行不能存在两个皇后，
+但是又正好是N个皇后又要放在N行上，这就意味着每一行都有且仅有一个皇后
+于是我们可以按照这样的方式来枚举这道题目的所有方案：依次枚举每一行皇后的位置，在这个枚举过程中确保不会在每一列、每一条斜线上出现两个皇后。
+
+那么如何“确保不会在每一列、每一条斜线上出现两个皇后”呢？我们可以仿照数独的方法，用3个数组来辅助计算：
+
+column[i]表示第i列是否已经存在皇后
+cross1[i]表示第i条左下-右上方向的斜线是否已经存在皇后
+cross2[i]表示第i条左上-右下方向的斜线是否已经存在皇后
+
+这下面的三行是最常见的pattern，先设置，调用递归函数后消除之前的设置
+current[i][j] = 'Q'; column[j] = true; cross1[i + j] = true; cross2[i - j + n - 1] = true;
+dfs(ans, current, i + 1, n, column, cross1, cross2);
+current[i][j] = '.'; column[j] = false; cross1[i + j] = false; cross2[i - j + n - 1] = false;
 
 /**
  * @param {number} n
  * @return {string[][]}
  */
-var solveNQueens = function (n) {
-    var ret = [];
-    var board = [];
-    for (var i = 0; i < n; i++) {
-        board.push(new Array(n).fill('.'));
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        // 构造一个初始棋盘
+        vector<string> current(n, string(n, '.'));
+        // 构造用于判断攻击的数组
+        vector<bool> column(n, false);
+        vector<bool> cross1(n * 2 - 1, false);
+        vector<bool> cross2(n * 2 - 1, false);
+        // 回溯求解所有的方案
+        dfs(ans, current, 0, n, column, cross1, cross2);
+        // 返回所有答案
+        return ans;
     }
-    helper(board, 0, ret);
-    return ret;
-};
-
-function helper(board, col, ret) {
-    if (col === board.length) {
-        ret.push(construct(board));
-    } else {
-        for (var i = 0; i < board.length; i++) {
-            if (check(board, i, col)) {
-                board[i][col] = 'Q';
-                helper(board, col + 1, ret);
-                board[i][col] = '.';
+    void dfs(vector<vector<string>>& ans, vector<string> current, int i, int n, vector<bool>& column, vector<bool>& cross1, vector<bool>& cross2) {
+        // 判断是否已经枚举完了每一行的皇后位置，如果是，说明已经找到了一组解
+        if (i == n) {
+            ans.push_back(current);
+        }
+        else {
+            // 否则枚举当前行皇后放置的位置
+            for (int j = 0; j < n; j++) {
+                // 判断是否会和之前放置的皇后产生列上的冲突
+                if (column[j]) continue;
+                // 判断是否会和之前放置的皇后产生第一种对角线上的冲突
+                if (cross1[i + j]) continue;
+                // 判断是否会和之前放置的皇后产生第二种对角线上的冲突
+                if (cross2[i - j + n - 1]) continue;
+                // 如果都不产生冲突，说明当前方案合法，对状态进行修改并递归的枚举下一行的放置方案
+                current[i][j] = 'Q'; column[j] = true; cross1[i + j] = true; cross2[i - j + n - 1] = true;
+                dfs(ans, current, i + 1, n, column, cross1, cross2);
+                // 回溯，消去产生的修改
+                current[i][j] = '.'; column[j] = false; cross1[i + j] = false; cross2[i - j + n - 1] = false;
             }
         }
     }
-}
+};
 
-function check(board, x, y) {
-    for (var i = 0; i < board.length; i++) {
-        for (var j = 0; j < y; j++) {
-            if (board[i][j] === 'Q' &&
-                (i === x || i + j === x + y || i + y === j + x)) return false;
-        }
-    }
-    return true;
-}
-function construct(board) {
-    return board.map(arr => arr.join(''));
-}
-console.log(solveNQueens(4));
