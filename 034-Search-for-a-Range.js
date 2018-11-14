a976f6080f738fd225919444f79fddb421934392
Huan Li <39723820+benli615349@users.noreply.github.com>
2018-11-14 23:23:26 +1100

    Update 034-Search-for-a-Range.js

--- 034-Search-for-a-Range.js
+++ 034-Search-for-a-Range.js
@@ -21,45 +21,41 @@
 因为是已经排序好的递增数组，那么使用两次二分法，
 第一次找到左边的区间，第二次找到右边的区间
 
-class Solution {
-public:
-    vector<int> searchRange(int A[], int n, int target) {
-       if(n == 0) {
-            return vector<int>({-1, -1});
-        }
+const searchRange = (nums, target) => {
+  let result = [-1, -1];
 
-        vector<int> v;
-        int low = 0;
-        int high = n - 1;
-        //第一次二分找第一个位置
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(A[mid] >= target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
+  if (nums.length === 0) {
+    return result;
+  }
 
-        if(low < n && A[low] == target) {
-            v.push_back(low);
-        } else {
-            return vector<int>({-1, -1});
-        }
-
-        low = low;
-        high = n - 1;
-        //从第一个位置开始进行第二次二分，找最后一个位置
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(A[mid] <= target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        v.push_back(high);
-        return v;
+  let left = 0;
+  let right = nums.length - 1;
+  let mid = 0;
+  while (left <= right) {
+    mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
+  }
+
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    mid = start + Math.floor((end - start) / 2);
+    if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  if (left <= end) {
+    result[0] = left;
+    result[1] = end;
+  }
+
+  return result;
 };
+
