ee857d9e4d45ef647bf9a228c833f90e1782b5bf
Ben Li <ben.li@qantas.com.au>
2018-11-14 18:21:41 +1100

    049

--- 049-group-anagrams.js
+++ 049-group-anagrams.js
@@ -14,4 +14,37 @@ Note:
 All inputs will be in lowercase.
 The order of your output does not matter.
 
+思路：
+首先设计一个map
+对于上面的输入，其结构是这样的：
+map = {
+	'aet': ['ate', 'eat', 'tea'],
+	'ant': ['nat', 'tan'],
+	'abt': ['bat']
+}
+对于数组中的每个元素，进行排序，然后看map是否存在该元素
+如果存在，说明该元素的变形已经在map中，执行push到map相应key的值
 
+
+const groupAnagrams = array => {
+  const map = new Map();
+  for (let i = 0; i < array.length; i++) {
+    let tmp = Array.from(array[i]);
+    tmp.sort();
+    let str = '';
+    tmp.forEach(item => str += item);
+    let value = [];
+    if (map.has(str)) {
+      value = map.get(str);
+    }
+    value.push(array[i]);
+    map.set(str, value);
+  }
+
+  let result = [];
+  for (let [key, value] of map) {
+    result.push(value);
+  }
+
+  return result;
+};
