854d2fdba907c7720d08800e3a48a33e94be0354
Ben Li <ben.li@qantas.com.au>
2018-12-29 00:00:51 +1100

    047

--- 047-Permutations-II.js
+++ 047-Permutations-II.js
@@ -16,32 +16,71 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function (nums) {
-    if (!nums.length) return [];
-    nums.sort((a, b) => a - b);
-    var res = [[]];
-    for (var i = 0; i < nums.length; i++) {
-        var len = res.length;
-        for (var j = 0; j < len; j++) {
-            var oldArr = res.shift();
-            if (i > 0 && nums[i] === nums[i - 1]) {
-                var k = oldArr.lastIndexOf(nums[i]);
-            } else {
-                k = 0;
-            }
-            for (; k <= oldArr.length; k++) {
 
-                if (k === oldArr.length || nums[i] !== oldArr[k]) {
-                    var newArr = oldArr.slice();
-                    newArr.splice(k, 0, nums[i]);
-                    // console.log(oldArr, newArr);
-                    res.push(newArr);
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+跟046相比有两个难题
+1，不能再使用array.includes(nums[i])的方式来保证每个元素只会被压入数组一次
+解决方法是不再直接push数组元素，而是push一个obj，该obj包含数组元素的index
+2，在dfs函数里，当条件满足时不能直接push到result数组，
+而是必须判断该数组是否在result数组里了
+解法方式是使用一个map，把每次要push的数组string化，比如
+map = {
+  '112': true,
+  '121': true
+}
+
+那么当数组已经在map里，就不push
+
+const dfs = (nums, array, map, result, n) => {
+    if (n === array.length) {
+       // 之前push的obj，所以需要提取出所需要的数组出来
+        const list = [];
+        for(let k = 0; k < n; k++) {
+            list.push(array[k].val)
+        }
+        const str = list.join('');
+        if(map[str]) return;
+        map[str] = true;
+        result.push([...list]);
+    } else {
+        for(let i = 0; i < n; i++) {
+            const obj = {
+                id: i,
+                val: nums[i]
+            }
+            let isIncluded = false;
+            for(let j = 0; j < array.length; j++) {
+                if (array[j].id === i) {
+                    isIncluded = true;
                 }
-
             }
+            if (isIncluded) continue;
+            array.push(obj);
+            dfs(nums, array, map, result, n);
+            array.pop()
         }
     }
-    return res;
+}
+
+var permuteUnique = function(nums) {
+    const result = [];
+    //使用n仅仅为了节省计算
+    const n = nums.length;
+    if (!nums || n === 0) {
+        return result.push([]);
+    }
+    
+    const array = [];
+    const map = {};
+    
+    dfs(nums, array, map, result, n);
+    
+    return result;
 };
 
-console.log(permuteUnique([1, 2, 2, 1]));
+
