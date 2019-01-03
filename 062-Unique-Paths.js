71bdb10f146ac87a0a29bf23813faa7b246133fa
Ben Li <ben.li@qantas.com.au>
2019-01-03 21:55:42 +1100

    062

--- 062-Unique-Paths.js
+++ 062-Unique-Paths.js
@@ -9,26 +9,31 @@
  *
  */
 
-/**
- * @param {number} m
- * @param {number} n
- * @return {number}
- */
-var uniquePaths = function (m, n) {
 
-    var arr = [];
-    for (var i = 0; i < m; i++) {
-        for (var j = 0; j < n; j++) {
-            if (i === 0 && j === 0) arr[0] = 1;
-            else {
-                var left = j - 1 < 0 ? 0 : arr[i * n + j - 1];
-                var top = i - 1 < 0 ? 0 : arr[(i - 1) * n + j];
-                arr[i * n + j] = left + top;
-            }
+典型的dp题
+由于机器人只能向下或者向右，设立dp[i][j]，其表示从[0][0]到[i][j]的路径数
+dp[i][j] = dp[i-1][j] + dp[i][j-1], 因为i，j这一点能且只能从其上面或者左面过来，所以把左边和上边两个点的dp值相加就可以了
 
+另外对于第一行dp[0][i]，其值一定是1，因为如果机器人和终点在同一行，而又其只能向右或者向下，那只能就有一个解，就是一直向右
+同理，对于dp[i][0]也一样
+
+var uniquePaths = function(m, n) {
+    const dp = [];
+    while(dp.push(new Array(m)) < n);
+    
+    for(let i = 0; i < m; i++) {
+        dp[0][i] = 1;
+    }
+    
+    for(let i = 0; i < n; i++) {
+        dp[i][0] = 1;
+    }
+    
+    for(let i = 1; i < n; i++) {
+        for(let j = 1; j < m; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
         }
     }
-    return arr[arr.length - 1];
+    
+    return dp[n-1][m-1]
 };
-
-console.log(uniquePaths(2, 2));
