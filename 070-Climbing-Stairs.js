afa396e95974b8e6ecf1944bad89b50dc33dbca2
Ben Li <ben.li@qantas.com.au>
2019-01-06 20:45:28 +1100

    070

--- 070-Climbing-Stairs.js
+++ 070-Climbing-Stairs.js
@@ -1,47 +1,42 @@
-/**
- * https://leetcode.com/problems/climbing-stairs/description
- * Difficulty:Easy
- *
- * You are climbing a stair case. It takes n steps to reach to the top.
- * Each time you can either climb 1 or 2 steps.
- * In how many distinct ways can you climb to the top?
- *
- * Note: Given n will be a positive integer.
- *
- * 答案:
- *
- * https://leetcode.com/articles/climbing-stairs/
- */
+You are climbing a stair case. It takes n steps to reach to the top.
 
-/**
- * @param {number} n
- * @return {number}
- */
-var climbStairs = function (n) {
-    if (n < 2) return 1;
-    return climbStairs(n - 1) + climbStairs(n - 2);
-};
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-/**
- * @param {number} n
- * @return {number}
- */
-var climbStairs2 = function (n) {
-    var ways = [1, 1];
-    for (var i = 2; i <= n; i++) {
-        ways[i] = ways[i - 1] + ways[i - 2];
+Note: Given n will be a positive integer.
+
+Example 1:
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+每次只能爬一格或者两格
+典型的dp
+dp[i]表示到i的unique ways
+i点只能通过两个方式到达，i-1点跳一格，i-2点跳两格，所以
+dp[i] = dp[i-1] + dp[i-2]
+
+var climbStairs = function(n) {
+    if (n <= 3) {
+        return n;
     }
-    return ways[n];
+    
+    const dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(let i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    
+    return dp[n];
 };
-
-console.log(climbStairs(1));
-console.log(climbStairs(2));
-console.log(climbStairs(3));
-console.log(climbStairs(4));
-
-console.log('==============');
-
-console.log(climbStairs2(1));
-console.log(climbStairs2(2));
-console.log(climbStairs2(3));
-console.log(climbStairs2(4));
