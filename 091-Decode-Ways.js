7f1a37be27f14d6a5ca1f0dcd08a346e5aeea992
Ben Li <ben.li@qantas.com.au>
2019-01-30 22:32:57 +1100

    091

--- 091-Decode-Ways.js
+++ 091-Decode-Ways.js
@@ -17,34 +17,102 @@
  */
 
 
-/**
- * @param {string} s
- * @return {number}
- */
-var numDecodings = function (s) {
-    if (!s.length) return 0;
-    var dp = [];
-    var n = s.length;
-    dp[0] = 1;
+跟070 climb stairs差不多的，i对应的值是由i-1和i-2决定的，这样的情况肯定是dp
 
-    for (var i = 1; i <= n; i++) {
-        dp[i] = 0;
-        var sum1 = parseInt(s[i - 1]);
-        var sum2 = parseInt(s[i - 2] + s[i - 1]);
+另外，只要解码失败的，都是返回0
+比如50，比如04，比如300
 
-        if (sum1 > 0 && sum1 < 10) {
-            dp[i] += dp[i - 1];
+这个意思是这样的，如果s[i]等于0，那么s[i-1]必须是1或者2，否则解码失败。
+如果s[i-1]是1或者2，那么s[i-1]s[i]这个是唯一的，所以dp[i] = dp[i-2]
+
+如果s[i]不是0，那么如果s[i-1]s[i]是在11到26之间，那么dp[i]= dp[i-1] + dp[i-2], 也就是说s[i-1]到s[i]只有一种解法，另外还要加上可能从s[i-2]直接跳两步到s[i]
+
+如果s[i-1]s[i]不是在11和26之间，那s[i-1]到s[i]只有一种解法，所以dp[i]跟dp[i-1]相等
+
+上面出现了dp[i-2]，所以循环要从i=2开始
+设置dp[i]表示0到i（包含i）有多少种解法
+
+所以需要首先计算出dp[0]和dp[1]的值
+var numDecodings = function(s) {
+    if (!s || s.length === 0 || s[0] === '0') {
+        return 0;
+    }
+    
+    const n = s.length;
+    
+    const dp = new Array(n).fill(1);
+        
+    let tmp = Number(s.substr(0, 2));
+    if (s[1] === '0') {
+        if (s[0] === '1' || s[0] === '2') {
+            dp[1] = 1;
+        } else {
+            return 0;
         }
-        if (sum2 > 9 && sum2 < 27) {
-            dp[i] += dp[i - 2];
+    } else {
+        if (tmp > 10 && tmp < 27) {
+            dp[1] = 2;
+        } else {
+            dp[1] = 1;
         }
     }
-    // console.log(dp);
-    return dp[n];
+    
+    for(let i = 2; i < n; i++) {
+        tmp = Number(s.substr(i-1, 2));
+        if (s[i] === '0') {
+           if (s[i-1] === '1' || s[i-1] === '2') {
+               dp[i] = dp[i-2]
+           } else {
+               return 0;
+           }
+        } else {
+            if (tmp > 10 && tmp < 27) {
+                dp[i] = dp[i-1] + dp[i-2]
+            } else {
+                dp[i] = dp[i-1]
+            }
+        }
+    }
+    
+    return dp[n-1]
 };
 
-console.log(numDecodings("0"));
-console.log(numDecodings("12"));
-console.log(numDecodings("10"));
-console.log(numDecodings("01"));
-console.log(numDecodings("100"));
+这里很明显可以看出来，应该把dp[1]也放入循环中来避免重复的逻辑
+
+上述的代码修改一个小地方就可以了
+如果是dp[i-2]担心过界的话，用
+dp[i-2] || dp[0]就可以了
+注意+的优先级高于||，所以
+dp[i] = dp[i-1] + (dp[i-2] || dp[0]);
+这个是需要括号的
+
+var numDecodings = function(s) {
+    if (!s || s.length === 0 || s[0] === '0') {
+        return 0;
+    }
+    
+    const n = s.length;
+    
+    const dp = new Array(n).fill(1);
+        
+    let tmp;
+    
+    for(let i = 1; i < n; i++) {
+        tmp = Number(s.substr(i-1, 2));
+        if (s[i] === '0') {
+           if (s[i-1] === '1' || s[i-1] === '2') {
+               dp[i] = dp[i-2] || dp[0];
+           } else {
+               return 0;
+           }
+        } else {
+            if (tmp > 10 && tmp < 27) {
+                dp[i] = dp[i-1] + (dp[i-2] || dp[0]);
+            } else {
+                dp[i] = dp[i-1]
+            }
+        }
+    }
+    
+    return dp[n-1]
+};
