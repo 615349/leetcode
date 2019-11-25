6c18dbb98328ca7569d845ba7d80157c7c39d554
Ben Li <ben.li@optus.com.au>
2019-11-25 21:32:11 +1100

    update 119

--- 119-pascal-triangle-II.js
+++ 119-pascal-triangle-II.js
@@ -1,3 +1,6 @@
+跟上题差不多，这次是求最后一行
+跟上题思路一样，加了一个小技巧，只要求左半边就可以了，右半边直接通过对成
+
 var getRow = function(rowIndex) {
     const result = [];
     
@@ -16,9 +19,10 @@ var getRow = function(rowIndex) {
         arr.push(1);
         const lastArrayIndex = result.length - 1;
         const lastArray = result[lastArrayIndex];
-        const lastArrayLength = lastArray.length;
-        for(let j = 1; j < lastArrayLength; j++) {
-            arr[j] = lastArray[j-1]+lastArray[j];
+        const halfLength = Math.floor(lastArray.length/2) + 1;
+        let j = 1;
+        for(; j < halfLength; j++) {
+            arr[lastArray.length - j] = arr[j] = lastArray[j-1]+lastArray[j];
         }
         arr.push(1);
         result.push([...arr]);
