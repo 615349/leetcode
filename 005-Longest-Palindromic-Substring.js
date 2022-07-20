da6defad9b7067404216d0730651c7a10c5ebbcb
Unicorn <39723820+615349@users.noreply.github.com>
2022-07-20 22:43:41 +1000

    Update 005-Longest-Palindromic-Substring.js

--- 005-Longest-Palindromic-Substring.js
+++ 005-Longest-Palindromic-Substring.js
@@ -19,37 +19,47 @@
  * @return {string}
  */
 
-必须使用DP解决问题，其他的没意思
 我们维护一个二维数组dp，其中dp[i][j]表示字符串区间[i, j]是否为回文串
 有且仅有三种情况
 i=j，那么只有一个字符，dp[i][j] = 1
 i + 1 = j，dp[i][j] = s[i] === s[j]
 j - i > 1, dp[i][j]的值取决于dp[i+1][j-1] && s[i] === s[j]
 
-var longestPalindrome = function(s) {
-    if (s === '' || s.length === 0) {
-        return ''
+const longestPalindrome = (s) => {
+  const n = s.length;
+  
+  const dp = [];
+  while(dp.push(new Array(n).fill(false)) < n);
+  
+  let maxLen = 0;
+  let result = '';
+  
+  for(let j = 0; j < n; j++) {
+    for(let i = 0; i <= j; i++) {
+      const distanceBetweenIAndJ = j - i + 1;
+      switch(distanceBetweenIAndJ) {
+        case 1:
+          // i 和 j 相等
+          dp[i][j] = true;
+          break;
+          
+        case 2:
+              // i 和 j 紧挨着
+          dp[i][j] = s[i] === s[j];
+          break;
+          
+        default:
+              // 其他情况，i 和 j 不紧挨着
+          dp[i][j] = dp[i+1][j-1] && s[i] === s[j];
+          break;
+      }
+      
+      if (dp[i][j] && maxLen < distanceBetweenIAndJ) {
+        maxLen = distanceBetweenIAndJ;
+        result = s.slice(i, j+1);
+      }
     }
-    
-    const n = s.length;
-    
-    let dp = [];
-    while(dp.push(new Array(n).fill(0)) < n);
-    
-    let i, j, maxLen = 0;
-    let result = '';
-    
-    for(i = 0; i < n; i++) {
-        for(j = 0; j <= i; j++) {
-            if (i === j) dp[i][j] = 1;
-            if ((i - j) === 1) dp[i][j] = s[i] === s[j];
-            if(i - j > 1) dp[i][j] = dp[i-1][j+1] && s[i] === s[j];
-            let distanceBetweenIandJ = i - j + 1;
-            if(dp[i][j] && maxLen < distanceBetweenIandJ) {
-                maxLen = distanceBetweenIandJ;
-                result = s.substring(j, i + 1)
-            }
-        }
-    }
-    return result;
-};
+  }
+  
+  return result;
+}
